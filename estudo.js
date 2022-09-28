c76b2b12dc93f4cf6a2aa138562e15e50f0f3ff0
FelpsziNdoTERMUX <106183643+felpszindojs091@users.noreply.github.com>
2022-09-28 17:40:18 -0300

    KK

---
+++ estudo.js
@@ -0,0 +1,18 @@
+console.log("Console: Alô Mundo...");
+alert("Alerta:KD minha nota do 2 trimestre?...");
+
+let tabuada = 2
+
+function meCHAME(){
+document.write("tabuada do felps" +tabuada + "<br>")
+    document.write(tabuada + " x 1 = " + (tabuada*1) +"<br>")
+    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>")
+    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>")
+    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>")
+    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>")
+    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>")
+    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>")
+    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>")
+    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>")
+    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>")
+}

f88fea49711bc6c221307740a462e06fc194489e
FelpsziNdoTERMUX <106183643+felpszindojs091@users.noreply.github.com>
2022-10-11 15:11:48 -0300

    kk

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Terceiro Trimestre
 
 ## Identificação
-Nome: Felipe   Nr. 6
+Nome: Jesus   Nr. 50
 
 ## Assuntos
 HTML, CSS e JavaScript
--- estudo.js
+++ estudo.js
@@ -16,3 +16,26 @@ document.write("tabuada do felps" +tabuada + "<br>")
     document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>")
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>")
 }
+
+
+function minhaTabuada(){
+for(var i=0 ; i <= 10; i++){
+    for(var j = 1; j <=10; j++){
+    document.write(i + " x " + j + " = " + (j*i) + "<br>");
+     }
+document.write("<br><br>")
+    }
+
+}
+
+function quadrado(){
+for(var i = 2; i < 21; i++)
+document.write("o quadrado de " + i + " é " + (i*i) + "<br>")
+}
+
+function total(){
+    let val = document.getElementById("valor").value;
+    let ju = document.getElementById("juros").value;
+    let resultado = (val * (1+ (ju/100)));
+    document.write("O resultado é " + resultado);
+}
